0c171991e2dfc028af3c6407c1e84c443d8c725c
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-15 20:35:37 -0500

    created a button that changes the background color

---
+++ js/button.js
@@ -0,0 +1,12 @@
+var changeBg = document.querySelector("#change-bg");
+
+var colors = ["pink", "green", "blue", "purple", "red"];
+var clicks = 0;
+
+changeBg.addEventListener('click', function() {
+    document.querySelector("body").style.backgroundColor = colors[clicks];
+  clicks++;
+  if (clicks === colors.length) {
+    clicks = 0;
+  }
+})
