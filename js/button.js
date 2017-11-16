cde11bbad0bf67eae102f2b66a2a46cac41e05f2
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-15 20:38:37 -0500

    added a readme file

--- js/button.js
+++ js/button.js
@@ -4,7 +4,7 @@ var colors = ["pink", "green", "blue", "purple", "red"];
 var clicks = 0;
 
 changeBg.addEventListener('click', function() {
-    document.querySelector("body").style.backgroundColor = colors[clicks];
+  document.querySelector("body").style.backgroundColor = colors[clicks];
   clicks++;
   if (clicks === colors.length) {
     clicks = 0;
